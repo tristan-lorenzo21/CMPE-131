a14bf77b86f04d8eeb1e7529f80652d1bd7f6f6f
tristan-lorenzo21 <tristan.lorenzo21@gmail.com>
2021-02-17 23:44:25 -0800

    Added files

---
+++ main.js
@@ -0,0 +1,10 @@
+var entry;
+var pass1 = "cmpe131";
+
+entry = prompt("Enter Password to View Page ", " ");
+
+if (entry = pass1)
+    alert('Correct password, click OK to enter');
+else {
+    window.location("Transcript.html")
+}
