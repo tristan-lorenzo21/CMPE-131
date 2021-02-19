8fa4156baeff88b52c2fe918fc9e973d2e9cfb91
tristan-lorenzo21 <tristan.lorenzo21@gmail.com>
2021-02-18 20:19:16 -0800

    Added dropdown menu capability

--- Homework1/main.js
+++ Homework1/main.js
@@ -1,10 +1,12 @@
+// Allows user to enter password and checks if it is correct or not
 var entry;
 var pass1 = "cmpe131";
 
-entry = prompt("Enter Password to View Page ", " ");
+entry = prompt("Enter Password to View Page: ");
 
-if (entry = pass1)
-    alert('Correct password, click OK to enter');
-else {
-    window.location("Transcript.html")
-}
+while (entry != pass1) {
+    alert("Password is incorrect");
+    entry = prompt("Enter Password to View Page: ");
+};
+
+alert("Password is correct!");
